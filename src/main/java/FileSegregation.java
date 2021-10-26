1b6d5db5bf08bbe22e34cd1508b7e59ddedbc3ce
Madzia <magda.gorska.lena@gmail.com>
2021-10-26 22:48:15 +0200

    refactoring

--- src/main/java/FileSegregation.java
+++ src/main/java/FileSegregation.java
@@ -3,10 +3,15 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class FileSegregation {
 
-    public static File createFolder(String path) {
+    public static File createFile(String path) {
 
         File file = new File(path);
 
@@ -17,19 +22,39 @@ public class FileSegregation {
         return file;
     }
 
-    public static void move(File file) throws IOException {
-        if (file.getPath().endsWith(".xml")) {
-            Files.move(Paths.get(String.format("c:\\HOME\\%s", file.getName())), Paths.get(String.format("c:\\HOME\\DEV\\%s", file.getName())), StandardCopyOption.REPLACE_EXISTING);
-        } else {
-            if (getDate(file) % 2 == 0) {
-                Files.move(Paths.get(String.format("c:\\HOME\\%s", file.getName())), Paths.get(String.format("c:\\HOME\\DEV\\%s", file.getName())), StandardCopyOption.REPLACE_EXISTING);
-            } else {
-                Files.move(Paths.get(String.format("c:\\HOME\\%s", file.getName())), Paths.get(String.format("c:\\HOME\\TEST\\%s", file.getName())), StandardCopyOption.REPLACE_EXISTING);
-            }
+    public static void segregation(File fileHOME) throws IOException {
+        if (fileHOME.exists() && fileHOME.isDirectory()) {
+            List<File> listOfFiles = Arrays.stream(fileHOME.listFiles())
+                    .filter(file -> file.getPath().endsWith(".jar") || file.getPath().endsWith(".xml"))
+                    .collect(Collectors.toList());
+
+            listOfFiles.forEach(System.out::println);
+            FileSegregationApp.fileCount.write("Liczba przeniesionych elementów: " + listOfFiles.size() + "\n");
+            listOfFiles.forEach(file -> System.out.println(file.getName()));
+            listOfFiles.forEach(file -> {
+                try {
+                    FileSegregation.move(file);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            FileSegregationApp.fileCount.close();
         }
     }
 
-    public static Integer getDate(File file) {
+    private static void move(File file) throws IOException {
+        var sourcePath = Paths.get(String.format("c:\\HOME\\%s", file.getName()));
+        var destinationPath = Paths.get(String.format("c:\\HOME\\DEV\\%s", file.getName()));
+
+        if (!file.getPath().endsWith(".xml") && (getDate(file) % 2 != 0)) {
+            destinationPath = Paths.get(String.format("c:\\HOME\\TEST\\%s", file.getName()));
+
+        }
+        Files.move(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
+    }
+
+
+    private static Integer getDate(File file) {
         Timestamp timestamp = new Timestamp(file.lastModified());
         var time = timestamp.toLocalDateTime().toLocalTime();
         return time.getHour();
--- src/main/java/FileSegregationApp.java
+++ src/main/java/FileSegregationApp.java
@@ -1,42 +1,21 @@
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.sql.Timestamp;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class FileSegregationApp {
 
-    private static String path_HOME = "c:\\HOME\\";
-    private static String path_DEV = "c:\\HOME\\DEV\\";
-    private static String path_TEST = "c:\\HOME\\TEST\\";
-
+    private static final String path_HOME = "c:\\HOME\\";
+    private static final String path_DEV = "c:\\HOME\\DEV\\";
+    private static final String path_TEST = "c:\\HOME\\TEST\\";
+    private static final String path_count = "c:\\HOME\\count.txt";
+    public static FileWriter fileCount;
 
     public static void main(String[] args) throws IOException {
 
-        File fileHOME = FileSegregation.createFolder(path_HOME);
-        File fileDEV = FileSegregation.createFolder(path_DEV);
-        File fileTEST = FileSegregation.createFolder(path_TEST);
+        File fileHOME = FileSegregation.createFile(path_HOME);
+        File fileDEV = FileSegregation.createFile(path_DEV);
+        File fileTEST = FileSegregation.createFile(path_TEST);
+        fileCount = new FileWriter(path_count);
 
-        if (fileHOME.exists() && fileHOME.isDirectory()) {
-            List<File> listOfFiles = Arrays.stream(fileHOME.listFiles())
-                    .filter(file -> file.getPath().endsWith(".jar") || file.getPath().endsWith(".xml"))
-                    .collect(Collectors.toList());
-
-            listOfFiles.forEach(System.out::println);
-            listOfFiles.forEach(file -> System.out.println(file.getName()));
-            listOfFiles.forEach(file -> {
-                try {
-                    FileSegregation.move(file);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-        }
-
-
+        FileSegregation.segregation(fileHOME);
     }
 
 
